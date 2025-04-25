647be014299e255359e33f361a7229917f21fd29
Asakura Mizu <asakuramizu111@gmail.com>
2025-04-26 01:23:44 +0800

    fix(hal): uspace feature cfg

--- modules/axhal/src/arch/x86_64/trap.rs
+++ modules/axhal/src/arch/x86_64/trap.rs
@@ -31,6 +31,7 @@ fn handle_page_fault(tf: &TrapFrame) {
 
 #[unsafe(no_mangle)]
 fn x86_trap_handler(tf: &mut TrapFrame) {
+    #[cfg(feature = "uspace")]
     super::tls::switch_to_kernel_fs_base(tf);
     match tf.vector as u8 {
         PAGE_FAULT_VECTOR => handle_page_fault(tf),
@@ -57,6 +58,7 @@ fn x86_trap_handler(tf: &mut TrapFrame) {
             );
         }
     }
+    #[cfg(feature = "uspace")]
     super::tls::switch_to_user_fs_base(tf);
 }
 
