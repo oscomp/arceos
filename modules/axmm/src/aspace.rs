f28657c42dbdf93c2b6e19eca8a5816c391708c0
OrangeQi-CQ <2082448790@qq.com>
2025-06-19 02:30:48 +0800

    fmt

--- modules/axmm/src/aspace.rs
+++ modules/axmm/src/aspace.rs
@@ -289,7 +289,10 @@ impl AddrSpace {
         match self.areas.find(vaddr) {
             Some(_) => {
                 self.pt.unmap(vaddr).map(|_| true).unwrap_or_else(|e| {
-                    panic!("FORCE UNMAP PAGE FAILED(PAGE TABLE FAILED) {:?}: {:#x}!", e, vaddr)
+                    panic!(
+                        "FORCE UNMAP PAGE FAILED(PAGE TABLE FAILED) {:?}: {:#x}!",
+                        e, vaddr
+                    )
                 });
             }
             _ => panic!("FORCE UNMAP PAGE FAILED(NOT FOUND AREA): {:#x}!", vaddr),
